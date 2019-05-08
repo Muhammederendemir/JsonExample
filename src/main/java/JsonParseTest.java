ed65bcafa20f896908ccaf028b3f3f4a07bd1b69
Muhammed Eren Demir <mhmmderen3@gmail.com>
2019-05-08 03:31:39 +0300

    Json parse işlemi eklendi.

--- src/main/java/JsonParseTest.java
+++ src/main/java/JsonParseTest.java
@@ -1,13 +1,11 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.LinkOption;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class JsonParseTest {
 
@@ -45,7 +43,6 @@ public class JsonParseTest {
         } catch (NullPointerException ex) {
             ex.printStackTrace();
         }
-
     }
 
 }
